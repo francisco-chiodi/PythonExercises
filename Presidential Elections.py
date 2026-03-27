1640492cd840f4619fb88b040095de2d6a9c3592
francisco-chiodi <franchiodi98@gmail.com>
2026-03-27 13:12:34 -0300

    first

--- Presidential Elections.py
+++ Presidential Elections.py
@@ -1,1 +1,17 @@
-#testing github
+#Dado el valor de los tres lados del triangulo, calcular su perimetro
+"""
+side_a = float(input("input the a side: "))
+side_b = float(input("input the b side: "))
+side_c = float(input("input the c side: "))
+sum_sides = side_a + side_b + side_c
+
+print('el perimetro es ', sum_sides)
+"""
+
+#Emloyee name and salary by ours
+
+name = "lain"
+ours = float (input("horas trabajadas: "))
+salary = float (input("salary: "))
+multiply = ours * salary
+print("the salary of the operator is " , multiply)
