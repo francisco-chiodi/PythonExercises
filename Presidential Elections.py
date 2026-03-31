bbe7e41c821854e6ad7ab50e1626ddc75b98fc6b
francisco-chiodi <franchiodi98@gmail.com>
2026-03-31 14:24:19 -0300

    promedy

--- Presidential Elections.py
+++ Presidential Elections.py
@@ -10,8 +10,20 @@ print('el perimetro es ', sum_sides)
 
 #Emloyee name and salary by ours
 
+"""
 name = "lain"
 ours = float (input("horas trabajadas: "))
 salary = float (input("salary: "))
 multiply = ours * salary
 print("the salary of the operator is " , multiply)
+"""
+#promedy of temperature
+temp_one = int (input ("input the first temp: "))
+temp_two = int (input ("input the second temp: "))
+temp_three = int (input ("input the third temp: "))
+
+temp_sum = temp_one + temp_two + temp_three
+promedy_one = temp_sum /3 #float division
+promedy_two = temp_sum // 3 #int division
+
+print("the interger promedy is ", promedy_two , "the float promedy is ", promedy_one) #promedy
