7b79ce18fb1e2fb6a348231b315ced4fc5ce3089
lain <lain@debian.lain>
2026-04-11 12:09:21 -0400

    test

--- Presidential Elections.py
+++ Presidential Elections.py
@@ -58,19 +58,59 @@ quantity = float(input("insert cash " ))
 print("$", quantity)
 print("pesos", quantity)
 """
-"""
 
+"""
 Duración de un vuelo
 Desarrollar un programa que, conociendo el horario de partida y llegada de un vuelo (hora y minutos), 
 determine cuál es su duración en minutos. Si el viajero necesita luego 45 minutos más para ir del 
 aeropuerto al hotel que ha reservado, ¿a qué hora llegara al mismo?
 """
-#flight time
-leave = input("input the flight time on hs:min format")
-arrive = input("input the flight arrive time on hs:min format")
-duration_leave = leave[0:1] * 60 + leave[4:5]
-duration_arrive = arrive[0:1] * 60 + leave
-extra_time = 45
-final_location = duration + (extra_time/60)
-print("the flight duration is ", duration)
-print("and you will arrive at your location at " , final_location)
+#TUPLE EXAMPLE
+"""
+sec = 4, 6
+a, b = sec
+print("a: ", a)
+print("b: ", b)
+"""
+"""
+a = 12
+b = 4
+c = a
+a = b
+b = c
+print(a)
+print(b)
+"""
+#FLIGHT TIME
+"""
+leave = input("input the flight time on hs:min format ")
+arrive = input("input the flight arrive time on hs:min format ")
+#duration_leave = int(leave[0:1]) , int(leave[4:5])
+duration_arrive = int(arrive[0:2]) - int(leave[0:2])
+total_mins = duration_arrive * 60 + int(arrive[4:6])
+extra_time = 0.45
+hotel_arrive = total_mins + extra_time
+print("the flight duration is ", total_mins)
+print("and you will arrive at your location at " , hotel_arrive)
+"""
+#ELECTORAL
+"""
+Control electoral
+Desarrollar un programa de control electoral en un centro vecinal, en el que se ingresen,
+para cierto candidato: apellido, nombre y cantidad de votos. Luego presentar en pantalla 
+un resumen que muestre: iniciales del candidato, cantidad de votos entre paréntesis, 
+y debajo una línea con tantas"x" como votos obtenidos (por ejemplo, el candidato obtuvo 4
+votos, deberá aparecer una línea como esta:  "xxxx"  con cuatro letras "x") (Asumimos que 
+en el centro vecinal nohay demasiados electores, de forma que podamos estar seguros que no
+habrá miles o millones de votos... sólo unos pocos para darle sentido al enunciado).
+"""
+"""
+name = input("your name is: ")
+last_name = input("your last name is ")
+votes = input("enter voters number ")
+a, b, c, d = votes[0], votes[1], votes[2], votes[3]
+
+
+
+print(name[0], last_name[0],"(", votes, ")\n")
+"""
