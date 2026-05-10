0c8c741819cdae459e109032544683ee4841a1e0
francisco-chiodi <franchiodi98@gmail.com>
2026-05-10 15:00:05 -0300

    test

--- armonic numbers.py
+++ armonic numbers.py
@@ -3,41 +3,56 @@
 # ESM armónico o ESM balanceado.
 
 """
-"Un número entero p se dice Equilibrado por Suma de Mitades (ESM) si la suma de los k dígitos de su primera mitad
- es igual a la suma de los k dígitos de la segunda mitad (Ejemplo: p = 234054 es ESM porque 2+3+4 = 9 = 0+5+4
-"""
-number = input('ingrese un numero de 6 digitos: ')
-number_int = int(number)
-half_one = int(number[0]) + int(number[1]) + int(number[2])
-half_two = int(number[3]) + int(number[4]) + int(number[5])
-inverted_number = number[5] + number[4] + number[3] + number[2] + number[1] + number[0]
-inverted_int = int(inverted_number)
-equilibrium_one = inverted_number[0:3]
-equilibrium_two = inverted_number[3:6]
-equilibrium = equilibrium_one == equilibrium_two
-rest = number_int - inverted_int
-
-if half_one == half_two:
-    print('Numero Equilibrado: ', number)
-    print('rest: ', rest)
-else:
-    print('Numero Inestable: ', number)
-    print('rest: ', rest)
-#si la diferencia entre p y su invertido pi es también ESM
-if rest == equilibrium:
-    print("equilibrado y armonico: ", rest)
-else:
-    print("no es equilibrado y armonico", rest)
-
+"Un número entero p se dice Equilibrado por Suma de Mitades (ESM) si la suma de los k dígitos de su
+ primera mitad
+ es igual a la suma de los k dígitos de la segunda mitad (Ejemplo: p = 234054 es ESM porque 2+3+4 = 9
+ = 0+5+4
 
 """
-if rest_half_life == rest_half_two:
-    print("Equilibardo y armonico: ", rest)
-else:
-    print("no es equilibrado y armonico")
-#equilibrium = half_one == half_two
+number = input("input the number: ")
 
+sum_1 = int(number[0]) + int(number[1]) + int(number[2])
+sum_2 = int(number[3]) + int(number[4]) + int(number[5])
+
+pi_1 = str(number)[5] + str(number)[4] + str(number[3]) + str(number[2]) + str(number[1]) + str(number[0])
+
+harmonic = int(number) - int(pi_1)
+harmonic_str = str(harmonic)
+
+if harmonic == 0:
+    harmonic_str = "000000"
+
+if str(harmonic_str)[0] == "-":
+    sum_harm_1 = int(harmonic_str[1]) + int(harmonic_str[2]) + int(harmonic_str[3])
+    sum_harm_2 = int(harmonic_str[4]) + int(harmonic_str[5]) + int(harmonic_str[6])
+
+else:
+    sum_harm_1 = int(harmonic_str[0]) + int(harmonic_str[1]) + int(harmonic_str[2])
+    sum_harm_2 = int(harmonic_str[3]) + int(harmonic_str[4]) + int(harmonic_str[5])
+
+
+#234054
+
+
+if sum_harm_1 == sum_harm_2:
+    print("Harmonic")
+elif sum_1 == sum_2:
+    print("ESM")
+else:
+    print("Unstable")
 """
+print(pi_1)
+print(harmonic)
+print(sum_harm_1)
+print(sum_harm_2)
+"""
+
+
+
+
+
+
+
 """
 c.) Un número entero Equilibrado ESM p, se dice también Armónico, si la diferencia entre p y su
  invertido pi es también ESM (Ejemplo: p = 1313. Es ESM (1+3 = 4 = 1+3). El invertido es pi = 3131.
