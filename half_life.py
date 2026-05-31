ba225df3c56be0825f7b935fdc81dfc5b5d0ac2e
francisco-chiodi <franchiodi98@gmail.com>
2026-05-31 15:39:15 -0300

    test

--- half_life.py
+++ half_life.py
@@ -1,18 +1,43 @@
 with open("tratamientos.txt" , "r") as file:
-    counter_test = 0
-    testing = 25
-    line_count = 0
-    line_count2 = 0
-    jump_count = 0
+
+    treatments = 0
+    counter_r2 = 0
+    counter_r3 = 0
+    counter_r4 = 0
+    counter_r5 = 0
+    counter_r6 = 0
+
+
+
+
 
     for line in file:
-        if 24 < len(line) :
-            line_count += 1
+        treatments += 1
+
+        if  len(line) >= 26:
+
+            descriptive_letter = line[25]
+
+            if descriptive_letter == "A":
+                counter_r2 += 1
+            elif descriptive_letter == "B":
+                counter_r3 += 1
+            elif descriptive_letter == "C":
+                counter_r4 += 1
+            elif descriptive_letter == "D":
+                counter_r5 +=1
+            elif descriptive_letter =="P":
+                counter_r6 +=1
+
+
+            #print(descriptive_letter) #jijo
+    print("r2", counter_r2)
+    print("r3", counter_r3)
+    print("r4", counter_r4)
+    print("r4", counter_r5)
+    print("r4", counter_r6)
 
 
 
-#if car = ' ' or car == '.' fin , if car! = ' ' n car! = '.'
-print(line_count)
-print(line_count2)
-print(line)
-    
+
+            #print(line)
--- word_sort.py
+++ word_sort.py
@@ -24,21 +24,25 @@ Palabras tenían al menos una vez la letra "x" o la letra "y": 2
 El promedio de letras por palabra en todo el texto es: 4.17
 Determinar cuántas palabras contuvieron sólo una vez la expresión "mo": 1
 
-
 """
 
 
 
 text = input("input the text: ")
 
-xy = lc = wc = l4 = 0
-have_xy = False
+xy = lc = add = wc = l4 = result = 0
+have_xy = False #pattern design by states.
 
 def is_xy(i):
     if i in "xy":
         return True
     else:
         return False
+
+def average(add,wc):
+    result = add//wc
+    return result #exit point of the function , it holds and send the value to "variable" at the end of the code
+                    #(place where is called)
 
 for i in text:
 
@@ -47,28 +51,32 @@ for i in text:
 
         if is_xy(i) == True:
 
-            have_xy = True
+            have_xy = True #lain from the wired says: like memory , ignores multiple coincidences once one is meet.
 
     else:
         if lc > 0:
             wc += 1
 
+            add = add + lc # is accumuled by trames, the lc=0 at the end dosent affect it.
             if have_xy == True:
-                xy += 1
+                xy += 1 #only counts the event, not the coincidence.
 
 
             if lc > 4:
                 l4  += 1
-        lc = 0
+
+        lc = 0 # this way only counts the letter by word not the total.
         have_xy = False
 
+variable = average(add,wc) #average(add,wc) is replaced by result that holds the value calculated on the function
 
 
 
 print("words", wc)
 print("words > 4", l4)
 print("xy words", xy)
-
+print("average", variable)
+#el mono momoxy toca el xilofon.
 """
 cl , cp , count_1 = 0
 vocal_in_3 = False
