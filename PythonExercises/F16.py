8ae76183e1dd4a8c51f765c47e4a9bebdbe62e45
francisco-chiodi <franchiodi98@gmail.com>
2026-09-03 11:16:36 -0300

    f16 file

---
+++ PythonExercises/F16.py
@@ -0,0 +1,8 @@
+"""
+1. Ordenar y buscar
+Se pide un programa que cargue n elementos numéricos aleatorios entre 1 y 100 inclusive (pueden existir duplicados). A partir de ese arreglo:
+
+Ordenarlo de forma ascendente y mostrarlo
+Buscar un elemento x dentro del arreglo (x se ingresa por teclado). Si no existe, informarlo. Si existe, determinar cuántos valores impares mayores a x se encontraron en el arreglo.
+
+"""
